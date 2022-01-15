29cd20747ceed8a304e7fc20724192d6e3d9ff8c
Fernando Oliva <fernandogermanoliva@gmail.com>
2022-01-15 18:33:53 -0300

    Se agrega código y apuntes (final) del vídeo 11.33 Instalando y Configurando Sequelize

---
+++ UpTaskNodeJS/config/db.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('uptasknode', 'root', 'root', {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  port: '3306',
+  operatorsAliases: false,
+  define: {
+    timestamps: false 
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
