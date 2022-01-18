894980731fa1910c5b6c355252557fa7a1209453
Fernando Oliva <fernandogermanoliva@gmail.com>
2022-01-18 09:20:53 -0300

    Cambio el puerto de la conexión por donde se ejecuta el servidor de MySQL de 3306 a 3360

--- UpTaskNodeJS/config/db.js
+++ UpTaskNodeJS/config/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('uptasknode', 'root', 'root', {
   host: '127.0.0.1',
   dialect: 'mysql',
-  port: '3306',
+  port: '3360',
   operatorsAliases: false,
   define: {
     timestamps: false 
